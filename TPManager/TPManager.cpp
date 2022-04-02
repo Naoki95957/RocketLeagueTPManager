86538ab12e3859335271811d9c19eb951fcadf64
Naoki <Naoki.l.95957@gmail.com>
2022-04-02 16:04:56 -0700

    first push

--- TPManager/TPManager.cpp
+++ TPManager/TPManager.cpp
@@ -9,6 +9,10 @@ std::shared_ptr<CVarManagerWrapper> _globalCvarManager;
 void TPManager::onLoad()
 {
 	_globalCvarManager = cvarManager;
+	// TODO listen to which GUI element is being edited at any given time -> 
+	// that way you only update 1 entity 
+	// instead of all entities
+
 	//cvarManager->log("Plugin loaded!");
 
 	//cvarManager->registerNotifier("my_aweseome_notifier", [&](std::vector<std::string> args) {
@@ -25,7 +29,7 @@ void TPManager::onLoad()
 
 	//cvar2.addOnValueChanged(std::bind(&TPManager::YourPluginMethod, this, _1, _2));
 
-	// enabled decleared in the header
+	////enabled decleared in the header
 	//enabled = std::make_shared<bool>(false);
 	//cvarManager->registerCvar("TEMPLATE_Enabled", "0", "Enable the TEMPLATE plugin", true, true, 0, true, 1).bindTo(enabled);
 
@@ -39,10 +43,77 @@ void TPManager::onLoad()
 	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", [this](std::string eventName) {
 	//	cvarManager->log("Your hook got called and the ball went POOF");
 	//});
-	// You could also use std::bind here
+	////You could also use std::bind here
 	//gameWrapper->HookEvent("Function TAGame.Ball_TA.Explode", std::bind(&TPManager::YourPluginMethod, this);
 }
 
 void TPManager::onUnload()
 {
+}
+
+std::vector<positionInfo> TPManager::getLocations()
+{
+	ServerWrapper gameState = gameWrapper.get()->GetCurrentGameState();
+
+	//list containing all moving objects
+	std::vector<positionInfo> allEntities = std::vector<positionInfo>();
+
+	//ball
+	allEntities.push_back({
+		"Ball",
+		gameState.GetBall().GetLocation(),
+		gameState.GetBall().GetRotation(),
+		gameState.GetBall().GetVelocity(),
+		gameState.GetBall().GetAngularVelocity()
+	});
+
+	// per car
+	ArrayWrapper cars = gameState.GetCars();
+	std::vector<CarWrapper> carVector = std::vector<CarWrapper>();
+	for (auto car : cars) {
+		carVector.push_back(car);
+	}
+
+	std::sort(carVector.begin(), carVector.end(), [](CarWrapper& a, CarWrapper& b) -> bool
+	{
+		return a.GetOwnerName() < b.GetOwnerName();
+	});
+
+	for (auto car : carVector) 
+	{
+		allEntities.push_back({
+			car.GetOwnerName(),
+			car.GetLocation(),
+			car.GetRotation(),
+			car.GetVelocity(),
+			car.GetAngularVelocity()
+		});
+	}
+
+	return allEntities;
+}
+
+void TPManager::setPositionInfo(positionInfo info)
+{
+	ServerWrapper gameState = gameWrapper.get()->GetCurrentGameState();
+
+	if (info.name == "Ball") {
+		BallWrapper ballW = gameState.GetBall();
+		ballW.SetLocation(info.location);
+		ballW.SetRotation(info.rotation);
+		ballW.SetVelocity(info.velocity);
+		ballW.SetAngularVelocity(info.angVelocity, false);
+	}
+	else
+	{
+		for (auto car : gameState.GetCars()) {
+			if (car.GetOwnerName() != info.name)
+				continue;
+			
+			car.SetLocation(info.location);
+			car.SetRotation(info.rotation);
+			car.SetVelocity(info.velocity);
+			car.SetAngularVelocity(info.velocity, false);
+		}
+	}
 }
--- TPManager/TPManager.h
+++ TPManager/TPManager.h
@@ -7,39 +7,46 @@
 #include "version.h"
 constexpr auto plugin_version = stringify(VERSION_MAJOR) "." stringify(VERSION_MINOR) "." stringify(VERSION_PATCH) "." stringify(VERSION_BUILD);
 
+BAKKESMOD_PLUGIN(TPManager, "TPManager", plugin_version, PLUGINTYPE_FREEPLAY)
+
+struct positionInfo
+{
+	std::string name;
+	Vector location;
+	Rotator rotation;
+	Vector velocity;
+	Vector angVelocity;
+};
 
 class TPManager: public BakkesMod::Plugin::BakkesModPlugin/*, public BakkesMod::Plugin::PluginSettingsWindow*//*, public BakkesMod::Plugin::PluginWindow*/
 {
-
 	//std::shared_ptr<bool> enabled;
 
 	//Boilerplate
 	virtual void onLoad();
 	virtual void onUnload();
 
-	// Inherited via PluginSettingsWindow
-	/*
-	void RenderSettings() override;
-	std::string GetPluginName() override;
-	void SetImGuiContext(uintptr_t ctx) override;
-	*/
+	std::vector<positionInfo> getLocations();
+	void setPositionInfo(positionInfo info);
 
-	// Inherited via PluginWindow
-	/*
+	//// Inherited via PluginSettingsWindow
+	//void RenderSettings() override;
+	//std::string GetPluginName() override;
+	//void SetImGuiContext(uintptr_t ctx) override;
 
-	bool isWindowOpen_ = false;
-	bool isMinimized_ = false;
-	std::string menuTitle_ = "TPManager";
+	//// Inherited via PluginWindow
 
-	virtual void Render() override;
-	virtual std::string GetMenuName() override;
-	virtual std::string GetMenuTitle() override;
-	virtual void SetImGuiContext(uintptr_t ctx) override;
-	virtual bool ShouldBlockInput() override;
-	virtual bool IsActiveOverlay() override;
-	virtual void OnOpen() override;
-	virtual void OnClose() override;
-	
-	*/
+	//bool isWindowOpen_ = false;
+	//bool isMinimized_ = false;
+	//std::string menuTitle_ = "TPManager";
+
+	//virtual void Render() override;
+	//virtual std::string GetMenuName() override;
+	//virtual std::string GetMenuTitle() override;
+	//virtual void SetImGuiContext(uintptr_t ctx) override;
+	//virtual bool ShouldBlockInput() override;
+	//virtual bool IsActiveOverlay() override;
+	//virtual void OnOpen() override;
+	//virtual void OnClose() override;
 };
 
